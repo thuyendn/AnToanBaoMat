c0dd212e3bb874346fcf10a6cf6294b1e4780602
Thuyendn <thuyennd21@gmail.com>
2025-09-11 15:21:21 +0700

    Update chương 4

--- BtChuong4.py
+++ BtChuong4.py
@@ -1,14 +1,13 @@
 def caesar_encrypt(text, k):
     result = ""
     for char in text:
-        if char.isalpha():  # chỉ mã hóa chữ cái
+        if char.isalpha():  
             base = ord('A') if char.isupper() else ord('a')
             result += chr((ord(char) - base + k) % 26 + base)
         else:
             result += char
     return result
 
-# Dữ liệu đề bài
 plaintext = "NguyenDoanThuyen"
 k = 8
 
